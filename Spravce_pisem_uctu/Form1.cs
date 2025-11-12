3f0d90b0ae58668473d2bb86e4a185b349cab37b
JakubDolezal07 <dolezaljakub.git@gmail.com>
2025-11-12 17:22:20 +0100

    Přidejte soubory projektu.

---
+++ Spravce_pisem_uctu/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Spravce_pisem_uctu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
